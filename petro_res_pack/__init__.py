34fcb95078b6da017a5600669028cd418a09f0bf
Lemikhov Alex <lemikhovalex@gmail.com>
2021-01-19 14:49:38 +0300

    this package is working now!

---
+++ petro_res_pack/__init__.py
@@ -0,0 +1,2 @@
+from petro_res_pack.src.env import Properties, PetroEnv, ResState
+from petro_res_pack.src.session import Session
---
+++ petro_res_pack/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='PetroResPack',
+    version='0.0.1',
+    author='Aleksandr Lemikhov',
+    author_email='lemikhovalex@gmail.com',
+    description='Package with gym-like env for petroleum reservoir simulation',
+    py_modules=['env', 'session'],
+    package_dir={'': 'src'}
+)
