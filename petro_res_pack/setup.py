89dbee7694269a01c833a6d5061722e6f976bd01
lemikhovalex <42550446+lemikhovalex@users.noreply.github.com>
2021-02-03 13:02:24 +0300

    Update setup.py
    like in https://uoftcoders.github.io/studyGroup/lessons/python/packages/lesson/

--- petro_res_pack/setup.py
+++ petro_res_pack/setup.py
@@ -2,10 +2,13 @@ from setuptools import setup
 
 setup(
     name='PetroResPack',
+    url='https://github.com/lemikhovalex/ReservoirModel',
     version='0.0.1',
     author='Aleksandr Lemikhov',
     author_email='lemikhovalex@gmail.com',
     description='Package with gym-like env for petroleum reservoir simulation',
     py_modules=['petro_env', 'petro_session'],
-    package_dir={'': 'src'}
+    package_dir={'': 'src'},
+    packages=['petro_res_pack'],
+    license='MIT'
 )
