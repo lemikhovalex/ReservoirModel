5a8cd5d113d9f613e062e1132977aa6dc10337db
Lemikhov Alex <lemikhovalex@gmail.com>
2021-06-25 12:10:31 +0300

    rm specific functions

--- petro_res_pack/petro_env.py
+++ petro_res_pack/petro_env.py
@@ -1,141 +1,9 @@
 import numpy as np
 import scipy.sparse as sparse
 import scipy.sparse.linalg as sp_linalg
-
-
-class Properties:
-    def __init__(self, nx=25, ny=25, k=1e-1 * 1.987e-13, dx=3, dy=3, phi=0.4, p_0=150 * 10 ** 5, d=10, dt=24316,
-                 s_0=0.4, c_w=1e-6, c_o=1e-6, c_r=3e-6, mu_w=1 / 1000., mu_o=15 / 1000., b_o=1., b_w=1., l_w=2., l_o=2.,
-                 s_wir=0.2, s_wor=0.8, k_rwr=0.1, k_rot=1., e_w=1., e_o=1., t_w=2., t_o=2.
-                 ):
-        # res propetis
-        self.nx = nx
-        self.ny = ny
-        self.k = k
-        self.dx = dx
-        self.dy = dy
-        self.phi = phi
-        self.p_0 = p_0
-        self.d = d
-        self.dt = dt
-        self.s_0 = {'w': 1 - s_0, 'o': s_0}
-        self.c = {'w': c_w, 'o': c_o, 'r': c_r}
-        self.mu = {'w': mu_w, 'o': mu_o}
-        self.b = {'w': b_w, 'o': b_o}
-        # relative saturation params
-        self.l_w = l_w
-        self.l_o = l_o
-        self.s_wir = s_wir
-        self.s_wor = s_wor
-        self.k_rwr = k_rwr
-        self.k_rot = k_rot
-        self.e_w = e_w
-        self.e_o = e_o
-        self.t_w = t_w
-        self.t_o = t_o
-        self.mask_close = np.ones(nx*ny)
-        for i in range(nx):
-            self.mask_close[ny * i] = 0
-
-    def get_s_wn(self, s_w):
-        s_wn = (s_w - self.s_wir) / (self.s_wor - self.s_wir)
-        if s_wn < 0:
-            s_wn = 0
-        if s_wn > 1:
-            s_wn = 1
-        return s_wn
-
-    def k_rel_w(self, s_w):
-        s_wn = self.get_s_wn(s_w)
-        out = s_wn ** self.l_w * self.k_rwr
-        out /= s_wn ** self.l_w + self.e_w * (1 - s_wn) ** self.t_w
-        return out
-
-    def k_rel_o(self, s_o):
-        s_w = 1 - s_o
-        s_wn = self.get_s_wn(s_w)
-        out = self.k_rot * (1 - s_wn) ** self.l_o
-        out /= (1 - s_wn) ** self.l_o + self.e_o * s_wn ** self.t_o
-        return out
-
-    def k_rel_ph_1val(self, s, ph):
-        out = 0
-        if ph == 'o':
-            out = self.k_rel_o(s)
-        elif ph == 'w':
-            out = self.k_rel_w(s)
-        return out
-
-    def k_rel_ph(self, s_1, s_2, p_1, p_2, ph):
-        """
-        1st floor then ceil
-        :param s_1:
-        :param s_2:
-        :param p_1:
-        :param p_2:
-        :param ph:
-        :return:
-        """
-        out = 0
-        if p_1 >= p_2:
-            out = self.k_rel_ph_1val(s=s_1, ph=ph)
-        elif p_1 <= p_2:
-            out = self.k_rel_ph_1val(s=s_2, ph=ph)
-        return out
-
-    def get_s_wn_ph_np(self, sat_arr: np.ndarray):
-        out = sat_arr - self.s_wir
-        out /= (self.s_wor - self.s_wir)
-        out = np.where(out >= 0, out, 0)
-        out = np.where(out < 1, out, 1)
-        return out
-
-    def k_rel_w_np(self, sat_arr: np.ndarray):
-        s_wn = self.get_s_wn_ph_np(sat_arr)
-        out = s_wn ** self.l_w * self.k_rwr
-        out /= s_wn ** self.l_w + self.e_w * (1 - s_wn) ** self.t_w
-        return out
-
-    def k_rel_o_np(self, s_o):
-        s_w = 1 - s_o
-        s_wn = self.get_s_wn_ph_np(s_w)
-        out = self.k_rot * (1 - s_wn) ** self.l_o
-        out /= (1 - s_wn) ** self.l_o + self.e_o * s_wn ** self.t_o
-        return out
-
-    def k_rel_ph_1val_np(self, s_arr, ph):
-        out = None
-        if ph == 'o':
-            out = self.k_rel_o_np(s_arr)
-        elif ph == 'w':
-            out = self.k_rel_w_np(s_arr)
-        return out
-
-
-def two_dim_index_to_one(i: int, j: int, ny: int) -> int:
-    return ny * i + j
-
-
-def one_d_index_to_two(one_d: int, ny: int):
-    i = int(one_d / ny)
-    j = one_d % ny
-    return i, j
-
-
-class ResState:
-    def __init__(self, values, bound_value, prop: Properties):
-        self.v = values
-        self.bound_v = bound_value
-        self.shape = values.shape
-        self.prop = prop
-
-    def __getitem__(self, item):
-        i, j = item
-        diag = two_dim_index_to_one(i, j, self.prop.ny)
-        if (i < 0) | (j < 0) | (i > self.prop.nx - 1) | (j > self.prop.ny - 1):
-            return self.bound_v
-        else:
-            return self.v[diag, 0]
+from .properties import Properties
+from .res_state import ResState
+from .utils import two_dim_index_to_one
 
 
 def get_lapl_one_ph_np(p: ResState, s, ph, prop: Properties):
@@ -323,6 +191,9 @@ class PetroEnv:
         self.s_star = 0
         self.set_s_star()
 
+        self.si_o = None
+        self.si_w = None
+
     def set_s_star(self):
         min_d = 100
         _s_star = 1
@@ -367,8 +238,8 @@ class PetroEnv:
             self.j_o *= self.openity
             self.j_w *= self.openity
         # now
-        self.lapl_w, si_w = get_lapl_one_ph_np(p=self.p, s=self.s_w, ph='w', prop=self.prop)
-        self.lapl_o, si_o = get_lapl_one_ph_np(p=self.p, s=self.s_o, ph='o', prop=self.prop)
+        self.lapl_w, self.si_w = get_lapl_one_ph_np(p=self.p, s=self.s_w, ph='w', prop=self.prop)
+        self.lapl_o, self.si_o = get_lapl_one_ph_np(p=self.p, s=self.s_o, ph='o', prop=self.prop)
 
         get_q_bound(self.p, self.s_w, 'w', self.prop, q_b=self.q_bound_w)
         get_q_bound(self.p, self.s_o, 'o', self.prop, q_b=self.q_bound_o)
@@ -496,3 +367,6 @@ class PetroEnv:
             s_check = self.s_o[well]
             action[_i] = 0. if s_check < self.s_star else 1.
         return action
+
+    def estimate_dt(self):
+        return self.prop.phi * self.dt_comp_sat.min() / (self.si_o + self.si_w)
