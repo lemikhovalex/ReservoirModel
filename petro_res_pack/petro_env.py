a4e3bcfe8dbab44851c70c6a4a9d3ebe89406534
Lemikhov Alex <lemikhovalex@gmail.com>
2021-06-29 14:54:56 +0300

    removed not nesseserely stuff and functiomn moved to other files

--- petro_res_pack/petro_env.py
+++ petro_res_pack/petro_env.py
@@ -1,135 +1,12 @@
 import numpy as np
 import scipy.sparse as sparse
 import scipy.sparse.linalg as sp_lin_alg
+
 from .properties import Properties
 from .res_state import ResState
-from .utils import two_dim_index_to_one
-
-
-def pad_with(vector, pad_width, iaxis, kwargs):
-    """
-    hard function from https://numpy.org/doc/stable/reference/generated/numpy.pad.html
-    used to extract sub matrix with padding
-    Args:
-        vector: 2d array, matrix
-        pad_width: size of pad
-        iaxis: dull, need for integration to np
-        kwargs: must include "pad_value"
-
-    Returns: nothing, inplace operator
-
-    """
-    pad_value = kwargs.get('pad_value', 10)
-    vector[:pad_width[0]] = pad_value
-    vector[-pad_width[1]:] = pad_value
-
-
-def get_sub_matrix(x: np.ndarray, k_size: int, center: tuple, pad_value: float) -> np.ndarray:
-    """
-    reci
-    Args:
-        x: 2d matrix, fro which the sub matrix will be extracted
-        k_size: size of square sub matrix
-        center: center of matrix, where from the sub matrix will be taken
-        pad_value: taking sub matrix of fixed size must be defined for close to frontier positions.
-                   values that are not in matrix, but required by indexing - padding
-    Returns: 2d sub matrix
-    """
-    x_padded_ = np.pad(x, k_size // 2, pad_with, pad_value=pad_value)
-    out = x_padded_[center[0]: center[0] + 2 * (k_size // 2) + 1,
-                    center[1]: center[1] + 2 * (k_size // 2) + 1
-                    ]
-    return out
-
-
-def get_laplace_one_ph_np(p: ResState, s, ph, prop: Properties):
-    s_b_test_x = np.ones((prop.nx, 1)) * s.bound_v
-    p_b_test_x = np.ones((prop.nx, 1)) * p.bound_v
-
-    s_x_ext = np.append(s.v.reshape((prop.nx, prop.ny)), s_b_test_x, axis=1)
-    s_x_ext = s_x_ext.reshape(-1)
-    s_x_ext = np.insert(s_x_ext, 0, s.bound_v)
-
-    p_x_ext = np.append(p.v.reshape((prop.nx, prop.ny)), p_b_test_x, axis=1)
-    p_x_ext = p_x_ext.reshape(-1)
-    p_x_ext = np.insert(p_x_ext, 0, p.bound_v)
-
-    '''
-    for i in range(len(p_x_ext) - 1):  # TODO this is a time consuming stuff
-        if p_x_ext[i] >= p_x_ext[i + 1]:
-            out_x[i] = s_x_ext[i]
-        else:
-            out_x[i] = s_x_ext[i + 1]
-    '''
-    comp_p_get_s = np.dtype({'names': ['p1', 'p2', 's1', 's2'],
-                             'formats': [np.double,
-                                         np.double,
-                                         np.double,
-                                         np.double]})
-    p_df = np.zeros(len(p_x_ext) - 1, dtype=comp_p_get_s)
-    p_df['p1'] = p_x_ext[:-1]
-    p_df['p2'] = p_x_ext[1:]
-
-    p_df['s1'] = s_x_ext[:-1]
-    p_df['s2'] = s_x_ext[1:]
-
-    out_x = np.where(p_df['p1'] >= p_df['p2'], p_df['s1'], p_df['s2'])
-
-    k_rel_x = prop.k_rel_ph_1val_np(out_x, ph)
-    # k_rel_x = np.array([prop.k_rel_ph_1val(s_o, ph) for s_o in out_x])  # consuming)
-    sigma = k_rel_x.max()
-    ##############################################
-    s_b_test_y = np.ones((1, prop.ny)) * s_b_test_x[0]
-    p_b_test_y = np.ones((1, prop.ny)) * p_b_test_x[0]
-
-    s_y_ext = np.append(s.v.reshape((prop.nx, prop.ny)), s_b_test_y, axis=0)
-    s_y_ext = s_y_ext.T.reshape(-1)
-    s_y_ext = np.insert(s_y_ext, 0, s.bound_v)
-
-    p_y_ext = np.append(p.v.reshape((prop.nx, prop.ny)), p_b_test_y, axis=0)
-    p_y_ext = p_y_ext.T.reshape(-1)
-    p_y_ext = np.insert(p_y_ext, 0, p.bound_v)
-
-    p_df['p1'] = p_y_ext[:-1]
-    p_df['p2'] = p_y_ext[1:]
-
-    p_df['s1'] = s_y_ext[:-1]
-    p_df['s2'] = s_y_ext[1:]
-
-    out_y = np.where(p_df['p1'] >= p_df['p2'], p_df['s1'], p_df['s2'])
-
-    k_rel_y = prop.k_rel_ph_1val_np(out_y, ph)
-    # k_rel_y = np.array([prop.k_rel_ph_1val(s_o, ph) for s_o in out_y])
-    sigma = min(sigma, k_rel_y.max())
-
-    # let's go diagonals
-    # main is first
-    # for x we need to drop first and last col
-    main_dia = np.zeros(prop.nx * prop.ny)
-    main_dia += np.delete(k_rel_x.reshape((prop.nx, prop.ny + 1)), obj=-1, axis=1).reshape(-1)
-    main_dia += np.delete(k_rel_x.reshape((prop.nx, prop.ny + 1)), obj=0, axis=1).reshape(-1)
-
-    main_dia += np.delete(k_rel_y.reshape((prop.ny, prop.nx + 1)), obj=-1, axis=1).T.reshape(-1)
-    main_dia += np.delete(k_rel_y.reshape((prop.ny, prop.nx + 1)), obj=0, axis=1).T.reshape(-1)
-
-    close_dia = k_rel_x.reshape((prop.nx, prop.ny + 1))
-    close_dia = np.delete(close_dia, obj=-1, axis=1)
-    close_dia = close_dia.reshape(-1)
-    close_dia *= prop.mask_close
-
-    dist_dia = k_rel_y.reshape((prop.ny, prop.nx + 1))
-    dist_dia = np.delete(dist_dia, obj=-1, axis=1)
-    dist_dia = np.delete(dist_dia, obj=0, axis=1)
-    dist_dia = dist_dia.T.reshape(-1)
-
-    lapl = sparse.diags(diagonals=[dist_dia, close_dia[1:],
-                                   -1 * main_dia,
-                                   close_dia[1:], dist_dia
-                                   ],
-                        offsets=[-1 * prop.ny, -1, 0, 1, prop.ny])  # .toarray()
-    lapl *= prop.k * prop.d * prop.dy / prop.mu[ph] / prop.dx
-    sigma *= prop.k * prop.d * prop.dy / prop.mu[ph] / prop.dx
-    return lapl, sigma
+from .parsing_utils import two_dim_index_to_one
+from .sub_matices_utils import get_sub_matrix
+from .math_phis_utils import get_laplace_one_ph
 
 
 def get_q_bound(p: ResState, s, ph, prop: Properties, q_b):
@@ -347,8 +224,8 @@ class PetroEnv:
             self.j_o *= self.openness
             self.j_w *= self.openness
         # now
-        self.lapl_w, self.si_w = get_laplace_one_ph_np(p=self.p, s=self.s_w, ph='w', prop=self.prop)
-        self.lapl_o, self.si_o = get_laplace_one_ph_np(p=self.p, s=self.s_o, ph='o', prop=self.prop)
+        self.lapl_w, self.si_w = get_laplace_one_ph(p=self.p, s=self.s_w, ph='w', prop=self.prop)
+        self.lapl_o, self.si_o = get_laplace_one_ph(p=self.p, s=self.s_o, ph='o', prop=self.prop)
 
         get_q_bound(self.p, self.s_w, 'w', self.prop, q_b=self.q_bound_w)
         get_q_bound(self.p, self.s_o, 'o', self.prop, q_b=self.q_bound_o)
@@ -389,12 +266,6 @@ class PetroEnv:
         elif ph == 'w':
             out = ((-1) * self.j_w * self.delta_p_vec).reshape((self.prop.nx, self.prop.ny))
         return out * self.openness.reshape((self.prop.nx, self.prop.ny))
-
-    def get_reward(self):
-        q_o = self.get_q('o')
-        q_w = self.get_q('w')
-
-        return (self.price['o'] * q_o.sum() - self.price['w'] * q_w.sum()) * self.prop.dt
 
     def reset(self):
         self.p.v = np.ones((self.prop.nx * self.prop.ny, 1)) * self.p.bound_v
